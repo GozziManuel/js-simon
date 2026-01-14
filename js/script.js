54ddedda1eb6cdd21dc328811ce7221eaae3f6c4
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-01-14 16:14:48 +0100

    added numberlist

--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
-const randomNumber = Math.floor(Math.random() * 50 + 1);
 let countDownStart = 5;
 const countDown = document.getElementById("countdown");
 const istruzioni = document.getElementById("instructions");
 const inputList = document.getElementById("answers-form");
+const numberlist = document.getElementById("numbers-list");
 
-// const counter = setInterval(() => {
-//   countDownStart--;
-//   countDown.innerHTML = `<p>${countDownStart}</p>`;
-//   if (countDownStart === 0) {
-//     clearInterval(counter);
-//     countDown.style.display = "none";
-//     istruzioni.style.display = "none";
-//     inputList.classList.remove("d-none");
-//   }
-// }, 1000);
+function numberGenerator(quantity, min, max) {
+  const numbers = [];
+  for (let i = 0; i < quantity; i++) {
+    const randomNumber = Math.floor(Math.random() * max + min);
+    numbers.push(randomNumber);
+  }
+  numberlist.innerHTML = `<li>${numbers.join("-")}</li>`;
+  return numbers;
+}
+numberGenerator(5, 1, 50);
+
+const counter = setInterval(() => {
+  countDownStart--;
+  countDown.innerHTML = `<p>${countDownStart}</p>`;
+  if (countDownStart === 0) {
+    clearInterval(counter);
+    countDown.style.display = "none";
+    istruzioni.style.display = "none";
+    inputList.classList.remove("d-none");
+  }
+}, 1000);
