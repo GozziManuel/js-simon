c8055d19b3cd63d740b7ac5b4765f3014590e229
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-01-14 15:53:02 +0100

    added transition to input

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 const randomNumber = Math.floor(Math.random() * 50 + 1);
 let countDownStart = 5;
 const countDown = document.getElementById("countdown");
+const istruzioni = document.getElementById("instructions");
+const inputList = document.getElementById("answers-form");
 
-setInterval(() => {
-  countDownStart--;
-  countDown.innerHTML = `<p>${countDownStart}</p>`;
-  if (countDownStart === 0) {
-  }
-}, 1000);
+// const counter = setInterval(() => {
+//   countDownStart--;
+//   countDown.innerHTML = `<p>${countDownStart}</p>`;
+//   if (countDownStart === 0) {
+//     clearInterval(counter);
+//     countDown.style.display = "none";
+//     istruzioni.style.display = "none";
+//     inputList.classList.remove("d-none");
+//   }
+// }, 1000);
